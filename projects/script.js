2f76408522e2ddf84d48aa968af2ecef44fa6d03
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2024-01-23 12:40:02 +0100

    add projects

---
+++ projects/script.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+
+    if (window.scrollY > 60) {
+      document.querySelector("#scroll-top").classList.add("active");
+    } else {
+      document.querySelector("#scroll-top").classList.remove("active");
+    }
+  });
+});
+
+// Change favicon
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible") {
+    document.title = "Progetti | Portfolio Pasquale Cicinelli";
+    $("#favicon").attr("href", "/assets/images/favicon.png");
+  } else {
+    document.title = "Torna indietro al Portfolio";
+    $("#favicon").attr("href", "/assets/images/favhand.png");
+  }
+});
+
+// fetch projects start
+function getProjects() {
+  return fetch("projects.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+// Create the document in html
+function showProjects(projects) {
+  let projectsContainer = document.querySelector(".work .box-container");
+  let projectsHTML = "";
+  projects.forEach((project) => {
+    projectsHTML += `
+        <div class="grid-item ${project.category}">
+        <div class="box tilt" style="width: 320px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${
+        project.image
+      }.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn ${
+      project.links.view ? "" : "disabled"
+    }" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${
+              project.links.code
+            }" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`;
+  });
+  projectsContainer.innerHTML = projectsHTML;
+
+  // <!-- tilt js effect starts -->
+  VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+  });
+  // vanilla tilt.js
+
+  // <!-- tilt js effect ends -->
+
+  /* ===== SCROLL REVEAL ANIMATION ===== */
+  const srtop = ScrollReveal({
+    origin: "bottom",
+    distance: "80px",
+    duration: 1000,
+    reset: true,
+  });
+
+  /* SCROLL PROJECTS */
+  srtop.reveal(".work .box", { delay: 400 });
+  srtop.reveal(".work .box", { interval: 200 });
+
+  // isotope filter products
+  var $grid = $(".box-container").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
+    masonry: {
+      columnWidth: 200,
+    },
+  });
+
+  // filter items on button click
+  $(".button-group").on("click", "button", function () {
+    $(".button-group").find(".is-checked").removeClass("is-checked");
+    $(this).addClass("is-checked");
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
+}
+
+// We have a pre-loader before mounting the page
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loader-container").style.display = "flex";
+
+  getProjects().then((data) => {
+    showProjects(data);
+    // Disattiva il pre-loader quando il caricamento è completo
+    document.getElementById("loader-container").style.display = "none";
+  });
+});
+// fetch projects end
+
+// disable developer mode
+document.onkeydown = function (e) {
+  if (e.key == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.key == "U".charCodeAt(0)) {
+    return false;
+  }
+};
