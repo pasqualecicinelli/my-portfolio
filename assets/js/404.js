d0fc9144eb6118ed45b591a55c5e6434dc94c733
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2024-02-06 19:51:09 +0100

    add toggleLanguage at 404 page

--- assets/js/404.js
+++ assets/js/404.js
@@ -3,6 +3,47 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $(".navbar").toggleClass("nav-toggle");
   });
+  //Opens the language drop-down menu by clicking
+  $(".dropdown").click(function () {
+    $(this).toggleClass("open");
+    $("#language").toggle(); // Toggle the display property
+  });
+});
+
+// TOGGLE LANGUAGE IT OR EN
+document
+  .querySelectorAll('[data-lang="en"]')
+  .forEach((element) => (element.style.display = "none"));
+var currentLanguage = "it";
+
+function setCurrentLanguage(lang) {
+  currentLanguage = lang;
+}
+
+function toggleLanguage() {
+  var elementsIt = document.querySelectorAll('[data-lang="it"]');
+  var elementsEn = document.querySelectorAll('[data-lang="en"]');
+
+  elementsIt.forEach(function (element) {
+    element.style.display = currentLanguage === "it" ? "" : "none";
+    element.style.opacity = currentLanguage === "it" ? "1" : "0";
+  });
+  elementsEn.forEach(function (element) {
+    element.style.display = currentLanguage === "en" ? "" : "none";
+    element.style.opacity = currentLanguage === "en" ? "1" : "0";
+  });
+  setCurrentLanguage(currentLanguage === "en" ? "it" : "en");
+}
+document.getElementById("language").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (event.target.tagName === "A") {
+    toggleLanguage();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", async function () {
+  toggleLanguage();
 });
 
 // disable developer mode
