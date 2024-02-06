bcd8a79a723462634803da154a04bfbccc24a196
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2024-02-06 09:38:36 +0100

    add toggleLanguage at experience page

--- experience/script.js
+++ experience/script.js
@@ -14,6 +14,11 @@ $(document).ready(function () {
       document.querySelector("#scroll-top").classList.remove("active");
     }
   });
+  //Opens the language drop-down menu by clicking
+  $(".dropdown").click(function () {
+    $(this).toggleClass("open");
+    $("#language").toggle(); // Toggle the display property
+  });
 });
 
 document.addEventListener("visibilitychange", function () {
@@ -26,16 +31,47 @@ document.addEventListener("visibilitychange", function () {
   }
 });
 
-//fetch experience start
-function getExperience() {
-  return fetch("experience.json")
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+// TOGGLE LANGUAGE IT OR EN
+document
+  .querySelectorAll('[data-lang="en"]')
+  .forEach((element) => (element.style.display = "none"));
+var currentLanguage = "it";
+
+function setCurrentLanguage(lang) {
+  currentLanguage = lang;
 }
 
-function showExperience(experience) {
+function toggleLanguage() {
+  var elementsIt = document.querySelectorAll('[data-lang="it"]');
+  var elementsEn = document.querySelectorAll('[data-lang="en"]');
+
+  elementsIt.forEach(function (element) {
+    element.style.display = currentLanguage === "it" ? "" : "none";
+    element.style.opacity = currentLanguage === "it" ? "1" : "0";
+  });
+  elementsEn.forEach(function (element) {
+    element.style.display = currentLanguage === "en" ? "" : "none";
+    element.style.opacity = currentLanguage === "en" ? "1" : "0";
+  });
+  setCurrentLanguage(currentLanguage === "en" ? "it" : "en");
+}
+document.getElementById("language").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (event.target.tagName === "A") {
+    toggleLanguage();
+    showExperience(currentLanguage === "en" ? "it" : "en");
+  }
+});
+
+//fetch experience start
+async function getExperience() {
+  const response = await fetch("experience.json");
+  const data = await response.json();
+  return data;
+}
+
+function showExperience(currentLanguage) {
   let experienceContainer = document.querySelector("#experience .timeline");
   let experienceHTML = "";
   let isRight = true;
@@ -48,9 +84,15 @@ function showExperience(experience) {
     <h2>${experience.company}</h2>
     </div>
     <div class="desc">
-      <h3>${experience.type}</h3>
-      <p>${experience.desc}</p>
-       <span> ${experience.place} ${experience.data}</span>
+      <h3>${
+        experience.type
+          ? experience.type[currentLanguage] || experience.type
+          : ""
+      }</h3>
+      <p>${experience.desc[currentLanguage]}</p>
+       <span> ${experience.place} ${
+      experience.data ? experience.data[currentLanguage] || experience.data : ""
+    }</span>
       </div>
   </div>
 </div>
@@ -58,29 +100,34 @@ function showExperience(experience) {
     isRight = !isRight; // Change the side for the next iteration
   });
   experienceContainer.innerHTML = experienceHTML;
+
+  /* ===== SCROLL REVEAL ANIMATION ===== */
+  const srtop = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 1000,
+    reset: true,
+  });
+
+  /* SCROLL EXPERIENCE */
+  srtop.reveal(".experience .timeline", { delay: 400 });
+  srtop.reveal(".experience .timeline .container", { interval: 400 });
 }
 
 // We have a pre-loader before mounting the page
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   document.getElementById("loader-container").style.display = "flex";
-  getExperience().then((data) => {
-    showExperience(data);
-    // Disattiva il pre-loader quando il caricamento è completo
-    document.getElementById("loader-container").style.display = "none";
-  });
-});
 
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-  origin: "top",
-  distance: "80px",
-  duration: 1000,
-  reset: true,
-});
+  experience = await getExperience("experience");
+  toggleLanguage();
+  showExperience("it");
+  // getExperience().then((data) => {
+  //   showExperience(data);
 
-/* SCROLL EXPERIENCE */
-srtop.reveal(".experience .timeline", { delay: 400 });
-srtop.reveal(".experience .timeline .container", { interval: 400 });
+  // Disattiva il pre-loader quando il caricamento è completo
+  document.getElementById("loader-container").style.display = "none";
+});
+// });
 
 // disable developer mode
 document.onkeydown = function (e) {
