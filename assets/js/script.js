56940df7da252b18684670915f492d916f495333
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2024-02-11 12:22:01 +0100

    edit animation starts when page elem is visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,39 @@ var typed = new Typed(".typing-text", {
 
 // <!-- typed js effect ends -->
 
+// Function to check if an element is visible and activate animation
+function isVisibleScroll(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+/* Function that takes the animation from "data-animation"
+   and adds the class for the animation */
+function checkAnimation() {
+  var animationElements = document.querySelectorAll("[data-animation]");
+  animationElements.forEach(function (element) {
+    var attrAnimation = element.getAttribute("data-animation");
+    var classAnimation = "animation-" + attrAnimation;
+    if (isVisibleScroll(element)) {
+      element.classList.add(classAnimation);
+    } else {
+      element.classList.remove(classAnimation);
+    }
+  });
+}
+
+// Add a listener for the scroll event
+window.addEventListener("scroll", checkAnimation);
+
+// Run the animation immediately if the element is already visible when the page loads
+document.addEventListener("DOMContentLoaded", checkAnimation);
+
 // Start language IT
 document
   .querySelectorAll('[data-lang="en"]')
@@ -337,3 +370,24 @@ srtop.reveal(".education .box", { interval: 200 });
 /* SCROLL EXPERIENCE */
 // srtop.reveal(".experience .timeline", { delay: 400 });
 // srtop.reveal(".experience .timeline .container", { interval: 400 });
+
+// Animation with set interval
+// document.addEventListener("DOMContentLoaded", function () {
+//   animation();
+// });
+// function animation() {
+//   var animationElements = document.querySelectorAll("[data-animation]");
+//   {
+//     animationElements.forEach(function (element) {
+//       var attrAnimation = element.getAttribute("data-animation");
+//       var classAnimation = "animation-" + attrAnimation;
+//       element.classList.add(classAnimation);
+//       setTimeout(function () {
+//         element.classList.remove(classAnimation);
+//       }, 6000);
+//     });
+//   }
+// }
+// setInterval(function () {
+//   animation();
+// }, 8000);
